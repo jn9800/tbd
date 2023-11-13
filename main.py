aed5dd31578a4a7151be03bd39a002b7b73e3bc9
jn9800 <jangrob3@gmail.com>
2023-11-13 12:51:50 +0100

    base settings for pygame with init

--- main.py
+++ main.py
@@ -10,3 +10,9 @@ framespersecond = 60
 pipe_image = "images/pipe.png"
 background_image = "images/background.png"
 bird_image = "images/bird.png"
+
+
+if __name__ == "__main__":  # aus geekforgeeks
+    # For initializing modules of pygame library
+    pygame.init()
+    framepersecond_clock = pygame.time.Clock()

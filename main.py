7f675b6365643df8c66b4dff5f98e93350c778ba
jn9800 <jangrob3@gmail.com>
2023-11-13 11:53:31 +0100

    first iteration of vars and icons for game base

--- main.py
+++ main.py
@@ -4,5 +4,9 @@ import pygame
 window_heigth = 600
 window_width = 400
 
-# generate window
-print("hallo")
+# generate window for frame
+window = pygame.display.set_mode((window_width, window_heigth))
+framespersecond = 60
+pipe_image = "images/pipe.png"
+background_image = "images/background.png"
+bird_image = "images/bird.png"

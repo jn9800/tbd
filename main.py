75112ac241999f8241a352bdb98800620a6f2952
jn9800 <jangrob3@gmail.com>
2023-11-20 10:30:36 +0100

    chaning velocity, for better playability

--- main.py
+++ main.py
@@ -35,7 +35,6 @@ if __name__ == "__main__":  # aus geekforgeeks
     framepersecond_clock = pygame.time.Clock()
 
 
-# Game loop
 running = True
 while running:
     # Check for events
@@ -44,7 +43,7 @@ while running:
             running = False
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_SPACE:
-                bird_velocity = -2  # birds upward speed
+                bird_velocity = -5  # birds upward speed
 
     # Update game state
     bird_position = (
@@ -52,7 +51,7 @@ while running:
         bird_position[1] + bird_velocity,
     )  # bird position ist nicht in der mitte Sven fragen
 
-    bird_velocity += 0.5  # bird drops each frame
+    bird_velocity += 0.1  # bird drops each frame
 
     if bird_position[1] < 0:
         bird_position: (bird_position[0], 0)

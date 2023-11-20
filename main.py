625fd2c0242323d9877e054f032c4646bced4386
jn9800 <jangrob3@gmail.com>
2023-11-20 09:16:54 +0100

    switch elevation from main into frame

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ window_width = 400
 # generate window for frame
 window = pygame.display.set_mode((window_width, window_heigth))
 framespersecond = 60
+elevation = window_heigth * 0.8
 pipe_image = "images/pipe.png"
 background_image = "images/background.png"
 bird_image = "images/bird.png"
@@ -16,4 +17,3 @@ if __name__ == "__main__":  # aus geekforgeeks
     # For initializing modules of pygame library
     pygame.init()
     framepersecond_clock = pygame.time.Clock()
-elevation = window_heigth * 0.8
